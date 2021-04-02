ca52c40046837eb885c07f15bc944f29ecbb7e47
AkimovIgor <akimovigor5417@gmail.com>
2021-04-02 13:59:39 +0300

    add files

---
+++ Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('exchanger1c::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('exchanger1c.name') !!}
+    </p>
+@endsection
